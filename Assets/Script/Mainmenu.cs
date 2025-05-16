cac23e6a4c30b1da2ff46098085d335177af5bca
Fadhilnugraha <aa.fadhil.nugraha@gmail.com>
2025-05-16 16:50:40 +0700

    Initial Unity project commit

---
+++ Assets/Script/Mainmenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Mainmenu : MonoBehaviour
+{
+    
+    Public void ButtonGoArtikel ()
+    {
+        SceneManager.LoadScene("Artikel");
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
